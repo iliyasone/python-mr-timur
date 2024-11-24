cf5e9e36142e451ece41b94c0fba8f476f647b67
iliyasone <iliyas.dzabbarov@gmail.com>
2024-11-24 07:10:53 +0300

    first comit

---
+++ a.py
@@ -0,0 +1,3 @@
+from b import super_important_value
+
+print(super_important_value)
---
+++ b.py
@@ -0,0 +1,8 @@
+super_important_value = 0
+
+print('some executable code in b')
+
+# if __name__ == '__main__':
+#     main()
+
+super_important_value = 16136
