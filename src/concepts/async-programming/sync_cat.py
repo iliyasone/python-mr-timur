5adb584cb946c11d1d17a5c3a0b7836f39a4ce71
iliyasone <iliyas.dzabbarov@gmail.com>
2025-07-12 19:14:11 +0300

    requests

--- src/concepts/async-programming/sync_cat.py
+++ src/concepts/async-programming/sync_cat.py
@@ -1,25 +1,40 @@
 import requests
-import json
+
 # API_KEY=""
+from typing import TypedDict
+
+
+class CatPicture(TypedDict, total=True):
+    id: str
+    url: str
+    """Url with a cat."""
+    width: int
+    height: int
 
 
 json_url = "https://api.thecatapi.com/v1/images/search?limit=10"
 url = "https://cdn2.thecatapi.com/images/"
 
 
-def get_cats_json():
+def get_cats_json() -> list[CatPicture]:
     response = requests.get(json_url)
     response.raise_for_status()
     with open("response.json", mode="w") as f:
-        json.dump(response.json(), f)
+        f.write(response.text)
+    return response.json()
+
+
+def json_analyzer_300(obj: list[dict]):
+    result: list[str] = []
+    for i in obj:
+        i["url"]
 
 
 def get_picture(url: str):
     response = requests.get(url)
     response.raise_for_status()
-    with open("MTc5NDIyMQ.jpg", 'wb') as file:
+    with open("MTc5NDIyMQ.jpg", "wb") as file:
         file.write(response.content)
-
 
 
 # eval('''exec("""import os;print(os.getenv("HELLO", "gs"))""")''')
