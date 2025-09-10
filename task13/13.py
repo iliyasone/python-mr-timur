14c8c4331e9eec68c9bef930d2dc0926384a939d
iliyasone <iliyas.dzabbarov@gmail.com>
2025-09-10 20:46:23 +0300

    lesson 10 sep

---
+++ task13/13.py
@@ -0,0 +1,13 @@
+# Сеть задана IP-адресом одного из входящих в неё узлов 191.128.66.83 и сетевой маской 255.192.0.0.
+
+ip = [191, 128, 66, 83]
+mask = [255, 192, 0, 0]
+
+network = []
+for i in range(4):
+    network.append(ip[i] & mask[i])
+
+print('mask', [bin(b) for b in mask])
+print('ip  ', [bin(b) for b in network])
+
+print(191, int('10111111', 2), int('11111111', 2), int('11111111', 2) - 1)
